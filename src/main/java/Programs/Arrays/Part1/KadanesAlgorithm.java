6f01c49264d6ae1c7c98bd5740eeb4f3286f0738
Manik <manik.setia001@gmail.com>
2022-07-18 10:03:27 +0530

    Added maximum subarray

--- src/main/java/Programs/Arrays/Part1/KadanesAlgorithm.java
+++ src/main/java/Programs/Arrays/Part1/KadanesAlgorithm.java
@@ -1,4 +1,50 @@
 package Programs.Arrays.Part1;
 
 public class KadanesAlgorithm {
+
+    public static int maxSubArray(int[] arr){
+
+        //Approach 1
+//        int max=Integer.MIN_VALUE;
+//        for(int i=0; i<arr.length; i++){
+//            for(int j=i; j<arr.length; j++){
+//                //to calculate sum from i to j
+//                int sum=0;
+//                for(int k=i; k<=j; k++){
+//                    sum += arr[k];
+//                }
+//                max=(sum>max)?sum:max;
+//            }
+//        }
+//        return max;
+
+        //Approach 2
+//        int max=Integer.MIN_VALUE;
+//        for(int i=0; i<arr.length; i++){
+//            int sum=0;
+//            for(int j=i; j<arr.length; j++){
+//                sum += arr[j];
+//                max=(sum>max)?sum:max;
+//            }
+//
+//        }
+//        return max;
+
+        //Approach 3: Kadane's algorithm
+        int sum=0;
+        int max=arr[0];
+        for(int i=0; i<arr.length; i++){
+            sum += arr[i];
+
+            max=(sum>max)?sum:max;
+            if(sum<0) sum=0;//as we want the maximum sum
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] nums={5,4,-1,7,8};
+        int max=maxSubArray(nums);
+        System.out.println("Maximum sum: "+max);
+    }
 }
