dd6a1d62fead2b44af071d2e3836a343924549fa
Manik <manik.setia001@gmail.com>
2022-07-21 20:31:30 +0530

    Added optimal approach

--- src/main/java/Programs/Arrays/Part2/RepeatAndMissing.java
+++ src/main/java/Programs/Arrays/Part2/RepeatAndMissing.java
@@ -36,7 +36,7 @@ public class RepeatAndMissing {
     //T.C. : O(n)
     //S.C. : O(n)
     public static ArrayList<Integer> repeatedNumberBetter(List<Integer> list){
-        int duplicateNumber=-1, repeatedNumber=-1;
+        int duplicateNumber=-1, missingNumber=-1;
         ArrayList<Integer> result=new ArrayList<>();
         Map<Integer, Integer> map=new HashMap<>(list.size());
         for(int i=1; i<=list.size(); i++){
@@ -50,11 +50,63 @@ public class RepeatAndMissing {
         //Now iterate over the map to find the repeated and the missing number
         for(Map.Entry<Integer, Integer> entry: map.entrySet()){
             if(entry.getValue()>=2) duplicateNumber=entry.getKey();
-            else if(entry.getValue()==0) repeatedNumber=entry.getKey();
+            else if(entry.getValue()==0) missingNumber=entry.getKey();
         }
 
         result.add(duplicateNumber);
-        result.add(repeatedNumber);
+        result.add(missingNumber);
+        return result;
+    }
+
+    //XOR method
+    //T.C. : O(n)
+    //S.C. : O(1)
+    public static ArrayList<Integer> repeatedNumberOptimal(List<Integer> list){
+        ArrayList<Integer> result=new ArrayList<>();
+        int xor=0;
+        for(Integer number: list) xor ^= number;
+        for(int i=1; i<=list.size(); i++) xor ^= i;
+
+        //Now we're left with xor of repeated and missing number. To separate these two, we need to find rsb
+        int rsb=xor & -xor;
+
+        int firstElement=0, secondElement=0;
+
+        //Now we've to perform AND operation of each element with rsb
+        for(Integer val: list){
+            if((rsb & val) == 0){
+                //means rsb of current element is set
+                firstElement ^= val;
+            }
+            else{
+                secondElement ^= val;
+            }
+        }
+
+        //We've to perform XOR with [1, n] as well because initially we got xor of two required numbers by using [1,n]
+        for(int i=1; i<=list.size(); i++){
+            if((rsb & i) == 0){
+                //means rsb of current element is set
+                firstElement ^= i;
+            }
+            else{
+                secondElement ^= i;
+            }
+        }
+
+        //Now task is to find which one is repeating and which one is missing
+        for(Integer val: list){
+            if(val==firstElement){
+                //duplicated is firstElement
+                result.add(firstElement);
+                result.add(secondElement);
+                break;
+            }
+        }
+
+        result.add(secondElement);
+        result.add(firstElement);
+
         return result;
     }
 
@@ -66,7 +118,8 @@ public class RepeatAndMissing {
         input.add(5);
         input.add(3);
 //        ArrayList<Integer> list=repeatedNumber(input);
-        ArrayList<Integer> list=repeatedNumberBetter(input);
+//        ArrayList<Integer> list=repeatedNumberBetter(input);
+        ArrayList<Integer> list=repeatedNumberOptimal(input);
         System.out.println(list);
     }
 }
