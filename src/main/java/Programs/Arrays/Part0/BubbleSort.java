b9779e802a6b72390f71831f117800f3b74f6db4
Manik <manik.setia001@gmail.com>
2022-07-22 20:20:01 +0530

    Added comment

--- src/main/java/Programs/Arrays/Part0/BubbleSort.java
+++ src/main/java/Programs/Arrays/Part0/BubbleSort.java
@@ -11,7 +11,7 @@ public class BubbleSort {
     }
 
     public static void bubbleSort(int[] arr, int n){
-        for(int i=0; i<n-1; i++){
+        for(int i=0; i<n-1; i++){//in worst case, loop will runs n-1 times.
             boolean swapped=false;//check if array gets sorted before the final iteration or array is already sorted
             for(int j=0; j<n-i-1; j++){
                 if(arr[j]>=arr[j+1]) {
