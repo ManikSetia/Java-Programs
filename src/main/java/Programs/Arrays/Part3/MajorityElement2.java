534fd4b1352240f1fde5fad41caa6ea3bba9a990
Manik <manik.setia001@gmail.com>
2022-07-25 21:09:38 +0530

    Added optimal approach

--- src/main/java/Programs/Arrays/Part3/MajorityElement2.java
+++ src/main/java/Programs/Arrays/Part3/MajorityElement2.java
@@ -23,19 +23,53 @@ public class MajorityElement2 {
 
         //T.C. : O(n)
         //S.C. : O(n)
+//        List<Integer> result=new ArrayList<>();
+//        Map<Integer, Integer> map=new HashMap<>();
+//        for(int ele: nums){
+//            map.put(ele, map.getOrDefault(ele, 0)+1);
+//        }
+//        for(Map.Entry<Integer, Integer> entry: map.entrySet()){
+//            if(entry.getValue()>nums.length/3) result.add(entry.getKey());
+//        }
+//        return result;
+
+
         List<Integer> result=new ArrayList<>();
-        Map<Integer, Integer> map=new HashMap<>();
+        int num1=-1, num2=-1;
+        int count1=0, count2=0;
+
         for(int ele: nums){
-            map.put(ele, map.getOrDefault(ele, 0)+1);
+            if(ele==num1) count1++;
+            else if(ele==num2) count2++;
+            else if(count1==0){
+                num1=ele;
+                count1=1;
+            }
+            else if(count2==0){
+                num2=ele;
+                count2=1;
+            }
+
+            else{
+                count1--;
+                count2--;
+            }
         }
-        for(Map.Entry<Integer, Integer> entry: map.entrySet()){
-            if(entry.getValue()>nums.length/3) result.add(entry.getKey());
+
+        //Since it's not mentioned that majority elements exist everytime. So, we've to add one more check.
+        count1=count2=0;
+        for(int ele: nums){
+            if(ele==num1) count1++;
+            else if(ele==num2) count2++;
         }
+        if(count1>nums.length/3) result.add(num1);
+        if(count2>nums.length/3) result.add(num2);
+
         return result;
     }
 
     public static void main(String[] args) {
-        int[] nums={3,2,3};
+        int[] nums={2,1,1,3,1,4,5,6};
         List<Integer> result=majorityElement(nums);
         System.out.println(result);
     }
