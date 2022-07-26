ca0245c1d8ef31a41c69c5b7856c123631b1a609
Manik <manik.setia001@gmail.com>
2022-07-26 13:43:55 +0530

    Added optimal2 approach

--- src/main/java/Programs/Arrays/Part3/SearchInA2DMatrix.java
+++ src/main/java/Programs/Arrays/Part3/SearchInA2DMatrix.java
@@ -19,12 +19,32 @@ public class SearchInA2DMatrix {
 
         //O(m+n)
         //Approach 3: Pick top right element and start comparing with the target because from the top right if you go to the left, elements st art decreasing and if you go to the down, elements start increasing.
-        int i=0, j=matrix[0].length-1;
-        while(i<matrix.length && j>=0){
-            if(matrix[i][j]==target) return true;
-            else if(matrix[i][j]>target) j--;
-            else i++;
+//        int i=0, j=matrix[0].length-1;
+//        while(i<matrix.length && j>=0){
+//            if(matrix[i][j]==target) return true;
+//            else if(matrix[i][j]>target) j--;
+//            else i++;
+//        }
+//        return false;
+
+
+        //Approach4: Think this matrix as a linear array with mXn elements.
+        // Use fake index to apply binary search.
+        //T.C. : log(mn)
+        int m=matrix.length, n=matrix[0].length;
+        int low=0, high=m*n-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+
+            //Now trick is to calculate row and column index from mid, so that we can get the element present at mid
+            int rowIndex=mid/n;
+            int columnIndex=mid%n;
+
+            if(matrix[rowIndex][columnIndex] == target) return true;
+            else if(matrix[rowIndex][columnIndex] > target) high=mid-1;
+            else low=mid+1;
         }
+
         return false;
     }
 
@@ -45,7 +65,7 @@ public class SearchInA2DMatrix {
                 {10, 11, 16, 20},
                 {23, 30, 34, 60}
         };
-        boolean result=searchMatrix(matrix, 23);
+        boolean result=searchMatrix(matrix, 233);
         System.out.println(result);
     }
 }
