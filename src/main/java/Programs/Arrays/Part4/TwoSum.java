62b76cb2a1791dede65d7f4a8d669a420244b0c0
Manik <manik.setia001@gmail.com>
2022-07-27 15:10:35 +0530

    Added brute force approach

---
+++ src/main/java/Programs/Arrays/Part4/TwoSum.java
@@ -0,0 +1,34 @@
+//Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+//
+//        You may assume that each input would have exactly one solution, and you may not use the same element twice.
+//
+//        You can return the answer in any order.
+
+
+package Programs.Arrays.Part4;
+
+import java.util.Arrays;
+
+public class TwoSum {
+
+    private static int[] twoSum(int[] nums, int target){
+        int[] result = new int[2];
+        for(int i=0; i<nums.length; i++){
+            for(int j=i+1; j<nums.length; j++){
+                if(nums[i]+nums[j]==target) {
+                    result[0]=i;
+                    result[1]=j;
+                }
+            }
+        }
+        return result;
+    }
+
+
+
+    public static void main(String[] args) {
+        int[] nums={3,3};
+        int[] result=twoSum(nums, 6);
+        System.out.println(Arrays.toString(result));
+    }
+}
