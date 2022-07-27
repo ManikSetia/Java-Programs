b894754fe736e140cca1a506ebd6d65e0a2ee7f7
Manik <manik.setia001@gmail.com>
2022-07-27 19:48:47 +0530

    Added better approach

--- src/main/java/Programs/Arrays/Part4/ThreeSum.java
+++ src/main/java/Programs/Arrays/Part4/ThreeSum.java
@@ -30,6 +30,48 @@ public class ThreeSum {
     }
 
 
+    //T.C. : O(n2logm)//logm is insertion time for set
+    //S.C. : O(m)+O(n)//for set and map
+    private static List<List<Integer>> threeSumBetter(int[] nums){
+        Set<List<Integer>> set=new HashSet<>();
+        List<Integer> list=null;
+        Map<Integer, Integer> map=new HashMap<>();
+
+        for(int ele: nums){
+            map.put(ele, map.getOrDefault(ele, 0)+1);
+        }
+
+        for(int i=0; i<nums.length; i++){
+
+            //Now, if we're taking nums[i] then we can't use it in the hash map for searching as it'll be exhausted. So, we've to decrease the occurrence in the hash map of current element by 1.
+            map.put(nums[i], map.get(nums[i])-1);
+
+            for(int j=i+1; j<nums.length; j++){
+
+                //Again same thing as above
+                map.put(nums[i], map.get(nums[j])-1);
+
+                int target=-(nums[i]+nums[j]);
+                if(map.containsKey(target) && map.get(target)>0){
+                    list=new ArrayList<>();
+                    list.add(target);
+                    list.add(nums[i]);
+                    list.add(nums[j]);
+                    Collections.sort(list);//so that we can get unique elements
+                }
+
+                map.put(nums[i], map.get(nums[j])+1);//putting back the occurrence
+            }
+
+            set.add(list);
+
+            map.put(nums[i], map.get(nums[i])+1);
+        }
+
+        List<List<Integer>> result=new ArrayList<>(set);
+
+        return result;
+    }
 
     public static void main(String[] args) {
         int[] nums={-1,0,1,2,-1,-4};
