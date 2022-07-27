f730f22ab6a64e809372e635078484fca246b904
Manik <manik.setia001@gmail.com>
2022-07-28 01:52:30 +0530

    Added better approach

--- src/main/java/Programs/Arrays/Part4/FourSum.java
+++ src/main/java/Programs/Arrays/Part4/FourSum.java
@@ -26,6 +26,8 @@ public class FourSum {
 //    }
 
     //Approach is first we'll sort the array to use pointers approach. Take 3 pointers (3 nested loop) and will search for forth element in the rest of the array using Binary Search.
+    //T.C. : O(n3logn)
+    //S.C. : O(n)
     private static List<List<Integer>> fourSum(int[] nums, int target){
         Arrays.sort(nums);
         List<Integer> list=null;
@@ -34,7 +36,7 @@ public class FourSum {
             for(int j=i+1; j<nums.length; j++){
                 for(int k=j+1; k<nums.length; k++){
                     list=new ArrayList<>();
-                    int sum=nums[i]+nums[j]+nums[k];
+                    long sum=(long)nums[i]+(long)nums[j]+(long)nums[k];
 
                     //Now we've to search for (-sum) in rest of the array. For that we're gonna use Binary Search
                     int forthElementIndex=binarySearch(nums, k+1, target-sum);
@@ -57,7 +59,7 @@ public class FourSum {
         return result;
     }
 
-    private static int binarySearch(int[] nums, int i, int search){
+    private static int binarySearch(int[] nums, int i, long search){
         int low=i;
         int high=nums.length-1;
         while(low<=high){
@@ -70,9 +72,55 @@ public class FourSum {
         return -1;
     }
 
+    //Approach is we'll use two pointers (2 Nested loops) for first two elements and then we'll iterate over rest of the array for (-sum of first two elements)
+    //T.C. : O(n3)
+    //S.C. : O(1)
+    private static List<List<Integer>> fourSumBetter(int[] nums, int target){
+        List<List<Integer>> result=new ArrayList<>();
+
+        if(nums==null || nums.length==0) return result;
+
+        Arrays.sort(nums);
+
+        for(int i=0; i<nums.length; i++){
+
+                for(int j=i+1; j<nums.length; j++){
+                    long sum1=(long)nums[i]+(long)nums[j];
+
+                    int low=j+1, high=nums.length-1;
+
+
+                    while(low<high){
+                        long sum2=(long)nums[low]+(long)nums[high];
+
+                        if(sum2>target-sum1) high--;
+                        else if(sum2<target-sum1) low++;
+                        else{
+                            result.add(Arrays.asList(nums[low], nums[high], nums[i], nums[j]));
+
+                            while(low<high && nums[low]==nums[low+1]) low++;
+                            while(low<high && nums[high]==nums[high-1]) high--;
+
+                            low++; high--;
+                        }
+                    }
+
+                    while(j<nums.length-1 && nums[j]==nums[j+1]) j++;
+
+                }
+                while(i< nums.length-1 && nums[i]==nums[i+1]) i++;
+
+
+        }
+
+
+        return result;
+    }
+
     public static void main(String[] args) {
-        int[] nums={2,2,2,2,2};
-        List<List<Integer>> list=fourSum(nums, 8);
+        int[] nums={1000000000,1000000000,1000000000,1000000000};
+//        List<List<Integer>> list=fourSum(nums, 8);
+        List<List<Integer>> list=fourSumBetter(nums, -294967296);
         System.out.println(list);
     }
 }
